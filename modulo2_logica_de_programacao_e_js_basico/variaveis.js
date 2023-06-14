d19a07a50eaed9f8d740f0b01e72307970d175d7
Juliana Souto <barutotec@gmail.com>
2023-06-14 10:58:07 -0300

    Aula sobre variáveis do JS.

---
+++ modulo2_logica_de_programacao_e_js_basico/variaveis.js
@@ -0,0 +1,10 @@
+let frase = 'Essa é uma frase qualquer';
+let fraseQualquer = 'Outra frase qualquer.';
+let frasequalquer = 'Frase qualquer.';
+
+const fraseQualquerInserida = 'frases';
+
+console.log(frase);
+console.log(fraseQualquer);
+console.log(frasequalquer);
+console.log(frases);
