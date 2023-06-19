6560f8bb86c85ef9fedf94a4b0aab37aa83bccf7
Juliana Souto <barutotec@gmail.com>
2023-06-18 21:13:34 -0300

    Aula sobre alguns Operadores Aritméticos.

---
+++ modulo2_logica_de_programacao_e_js_basico/operadores_aritmeticos.js
@@ -0,0 +1,26 @@
+// ###### OPERADORES ARITMÉTICOS ######
+const numero1 = 13;
+const numero2 = 27;
+
+const adicao = numero1 + numero2 //= 40
+console.log(adicao);
+
+const subtracao = numero1 - numero2 //= -14
+console.log(subtracao);
+
+const multiplicacao = numero1 * numero2 //=351
+console.log(multiplicacao);
+
+const divisao = numero1 / numero2 //= 0.481481481
+const modulo = numero2 % numero1 //= 1
+console.log(divisao);
+console.log(modulo);
+
+const exponencial = numero1 ** numero2 //Seria 13 elevado por 27 = 1.192533292512492e+30
+console.log(exponencial);
+
+const incremento = numero1++ //= 14
+const decremento = numero2-- //= 26
+
+
+
