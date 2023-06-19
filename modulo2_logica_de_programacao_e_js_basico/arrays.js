6a3f5cf48a1a0792de0905a109de1c31121ce90d
Juliana Souto <barutotec@gmail.com>
2023-06-18 21:08:32 -0300

    Aula sobre Arrays.

---
+++ modulo2_logica_de_programacao_e_js_basico/arrays.js
@@ -0,0 +1,27 @@
+// ###### ARRAYS ######
+// É uma lista repleta de valores.
+// Sendo uma lista única é chamada de vetor. Sendo uma lista de outras listas é chamada de matriz.
+// É utilizado colchetes para declarar uma lista.
+// Cada item da lista tem posição específica, e no caso dos arrays, as posições começam exatamente pelo zero (0), denominada Index.
+
+// Exemplo de lista de vetor
+const vetor = ['Juliana', 'Julia', 'Lucas', 'Guinha', 'Clara', 'Mike', 13, 27, true, false, {}, null];
+
+console.log(vetor);
+console.log(vetor[0]);
+console.log(vetor[1]);
+console.log(vetor[6]);
+console.log(vetor[7]);
+
+// Exemplo de lista matriz
+const matriz = [
+    ['Celta', 'Spin', 'S10', 'Gol', 'Nivus'],
+    ['Juliana', 'José Carlos', 'Nilo', 'Jackson', 'Jacqueline'],
+    ['Desenvolvedora Front-End', 'Aposentado', 'Farmacêutico', 'Motorista', 'Do lar']
+]
+
+console.log(matriz);
+console.log(matriz[0]);
+console.log(matriz[1]);
+console.log(matriz[2]);
+
