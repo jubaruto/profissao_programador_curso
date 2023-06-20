77c17636bdc3ca79f60d53fa691c414c20f819e3
Juliana Souto <barutotec@gmail.com>
2023-06-19 23:28:15 -0300

    Aula sobre os Operadores de Atribuição.

---
+++ modulo2_logica_de_programacao_e_js_basico/operadores_de_atribuicao.js
@@ -0,0 +1,15 @@
+// ###### OPERADORES DE ATRIBUIÇÃO ######
+// Atribui um valor a algo.
+// Exemplos de Operadores de Atribuição:
+// Sinal de igual (=) -> atribui um valor a uma propriedade;
+// Sinal de adição (+) juntamente com o sinal de igual (=) -> irá somar e em seguida atribuir;
+// Sinal de subtração (-) juntamente com o sinal de igual (=) -> irá diminuir e em seguida atribuir;
+// Sinal de multiplicação (*) juntamente com o sinal de igual (=) -> irá multiplicar e em seguida atribuir;
+
+const numero1 = 13;
+const numero2 = 27;
+const numero3 = numero1 += numero2 //numero1 = numero1 + numero2
+const numero4 = numero1 -= numero2 //numero1 = numero1 - numero2
+const numero5 = numero1 *= numero2 //numero1 = numero1 * numero2
+
+
