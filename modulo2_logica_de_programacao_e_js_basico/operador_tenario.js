c43203cd7ec0e169b18c827eda120b126082f958
Juliana Souto <barutotec@gmail.com>
2023-06-19 23:23:41 -0300

    Aula sobre Operador Tenário.

---
+++ modulo2_logica_de_programacao_e_js_basico/operador_tenario.js
@@ -0,0 +1,18 @@
+// ###### OPERADOR TENÁRIO ######
+// É uma condicional, ou seja, utilizado para realizar tomada de decisões.
+
+const numero1 = 13;
+const numero2 = 27;
+const numero3 = 0;
+const numero4 = '';
+const numero5 = ' ';
+
+console.log(true ? 'Retornou true' : 'Retornou false');
+console.log(false ? 'Retornou true' : 'Retornou false');
+console.log(numero1 ? 'Retornou true' : 'Retornou false');
+console.log(numero3 ? 'Retornou true' : 'Retornou false');
+console.log(numero4 ? 'Retornou true' : 'Retornou false');
+console.log(numero5 ? 'Retornou true' : 'Retornou false');
+
+numero1 ? alert('retornou true') : alert('retornou false');
+numero4 ? alert('retornou true') : alert('retornou false');
