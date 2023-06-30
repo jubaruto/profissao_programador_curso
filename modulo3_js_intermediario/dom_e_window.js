621b4f68a3a08e10204d992ae4b1ee1319a2b5e1
Juliana Souto <barutotec@gmail.com>
2023-06-29 21:24:35 -0300

    Aula sobre DOM & WINDOW.

---
+++ modulo3_js_intermediario/dom_e_window.js
@@ -0,0 +1,6 @@
+//###### DOM ######
+//DOM (Document Object Model): É uma estrutura que representa os documentos HTML e XML. É possível alterar a estrutura HTML, utilizando JS - adicionar uma nova tag, modificar a cor de fundo de algo e etc. A estilização também pode ser modificada. A DOM é basicamente uma árvore de elementos que representam a página do browser e conseguimos acessar, por padrão, utilizando JS.
+//WINDOW -> É o objeto principal, fazendo referencia a toda janela do browser. Dentro do WINDOW teremos os:
+//DOCUMENT -> Faz referencia ao documento HTML;
+//LOCATION -> Faz referência a URL, Path e etc;
+//HISTORY -> Representa o histórico de navegação da sessão. Basicamente, é o objeto que faz o controle e contém os métodos e propriedades responsáveis pela navegação da sessão;
