ea8d40658d1d6b42d5455132df03641a4e621cdd
Juliana Souto <barutotec@gmail.com>
2023-06-30 20:16:58 -0300

    Aula sobre Módulos (importar e exportar).

---
+++ modulo3_js_intermediario/modules2.js
@@ -0,0 +1,17 @@
+//Exemplo 01
+export function alerta(){
+    alert('Alerta Aqui!');
+}
+
+//Exemplo 02
+export function alerta(alerta){
+    alert(alerta);
+}
+
+export const textoAlerta = 'Esse é o novo texto do Alerta.';
+
+//Exemplo 03
+export default function alerta(alerta){
+    alert('alerta');
+}
+
